f303724caa07e5e952dddc80d675566e22f73fe5
Abdullah <80760611+abdullah-rgb@users.noreply.github.com>
2022-08-05 16:21:35 +0300

    Add files via upload

---
+++ artix-install-runit/artix-install.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+clear
+
+# edit /etc/pacman.conf file
+nano /etc/pacman.conf
+
+clear
+
+# disk configuration
+lsblk
+echo ' '
+read -p 'Select disk: ' disk_selection
+cfdisk $disk_selection
+
+clear
+lsblk
+echo ' '
+
+# format disk label
+read -p 'Root partition with /dev, /dev/???: ' root_partition
+mkfs.ext4 $root_partition
+mount $root_partition /mnt
+
+read -p 'Boot partition with /dev, /dev/???: ' boot_partition
+mkfs.fat -F32 $boot_partition
+mkdir -p /mnt/boot/efi
+
+clear
+
+# install base packages
+basestrap -i /mnt base base-devel linux linux-headers linux-firmware runit git nano
+
+# create fstab file
+fstabgen -U /mnt > /mnt/etc/fstab
+mount $boot_partition /mnt/boot/efi
+
+cp $(pwd)/chroot.sh /mnt
+artix-chroot /mnt /bin/bash
+rm -rf /mnt/chroot.sh
---
+++ artix-install-runit/chroot.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+clear
+
+# edit /etc/pacman.conf file
+nano /etc/pacman.conf
+
+clear
+
+# timezone
+echo 'Example: Europe/Istanbul, Europe/Zurich, America/New_York'
+read -p 'Timezone: ' time_zone
+ln -sf /usr/share/zoneinfo/$time_zone /etc/localtime
+hwclock --systohc
+
+clear
+
+# locale
+echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
+locale-gen
+
+touch /etc/locale.conf
+echo 'LANG=en_US.UTF-8' >> /etc/locale.conf
+
+clear
+
+# host
+read -p 'Hostname: ' host_name
+echo $host_name >> /etc/hostname
+
+echo '127.0.0.1       localhost' >> /etc/hosts
+echo '::1             localhost' >> /etc/hosts
+echo '127.0.1.1       '$host_name'.localdomain     '$host_name >> /etc/hosts
+
+clear
+
+# root password
+echo 'Root password'
+passwd
+
+clear
+
+# packages
+pacman -Sy networkmanager networkmanager-runit
+clear
+echo 'After restarting the computer, enter the command "sudo ln -s /etc/runit/sv/NetworkManager /run/runit/service ; sudo sv start NetworkManager'
+sleep 6
+clear
+
+# touchpad support
+pacman -S xf86-input-libinput
+clear
+
+# NTFS filesystem support
+pacman -S ntfs-3g
+clear
+
+# GRUB
+pacman -S grub efibootmgr os-prober
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id='Artix Linux'
+grub-mkconfig -o /boot/grub/grub.cfg
+sleep 3
+clear
+
+# useradd
+read -p 'Username: ' user_name
+useradd -m -g users -G wheel,storage,power,audio,video -s /bin/bash $user_name
+echo $user_name 'password'
+passwd $user_name
+
+# sudo privileges
+EDITOR=nano visudo
+clear

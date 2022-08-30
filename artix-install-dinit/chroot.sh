0aa7e6aed1eaf3bd7b5545f98bfd3595189239af
Abdullah <80760611+abdullah-rgb@users.noreply.github.com>
2022-08-30 15:18:05 +0300

    Update chroot.sh

--- artix-install-dinit/chroot.sh
+++ artix-install-dinit/chroot.sh
@@ -15,11 +15,21 @@ hwclock --systohc
 clear
 
 # locale
-echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
-locale-gen
 
-touch /etc/locale.conf
-echo 'LANG=en_US.UTF-8' >> /etc/locale.conf
+read -p "Select locale [en/tr]: " localeset
+
+if [ $localeset == "en" ];
+then
+    echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+    locale-gen
+    touch /etc/locale.conf
+    echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+else
+    echo "tr_TR.UTF-8 UTF-8" >> /etc/locale.gen
+    locale-gen
+    touch /etc/locale.conf
+    echo "LANG=tr_TR.UTF-8" >> /etc/locale.conf
+fi
 
 clear
 

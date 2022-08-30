da5d3be06b0d0f4948584cc14758e82bf2d19ec4
Abdullah <80760611+abdullah-rgb@users.noreply.github.com>
2022-08-30 15:20:05 +0300

    Update chroot.sh

--- artix-install-runit/chroot.sh
+++ artix-install-runit/chroot.sh
@@ -15,11 +15,24 @@ hwclock --systohc
 clear
 
 # locale
-echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
-locale-gen
 
-touch /etc/locale.conf
-echo 'LANG=en_US.UTF-8' >> /etc/locale.conf
+read -p "Select locale [en/tr]: " localeset
+
+if [ $localeset == "en" ];
+then
+    echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+    locale-gen
+    touch /etc/locale.conf
+    echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+    touch /etc/vconsole.conf
+    echo "KEYMAP=us" >> /etc/vconsole.conf
+else
+    echo "tr_TR.UTF-8 UTF-8" >> /etc/locale.gen
+    locale-gen
+    touch /etc/locale.conf
+    echo "LANG=tr_TR.UTF-8" >> /etc/locale.conf
+    touch /etc/vconsole.conf
+    echo "KEYMAP=trq" >> /etc/vconsole.conf
 
 clear
 
